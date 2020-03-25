184128f2721c40d68ee339fb33929ea49f5ba2bf
Vinni - <vinni9@gmail.com>
2020-03-25 10:30:15 -0500

    Update DCM.java

--- Backend/servRegistrarTarjeta/src/java/co/dpenad3/utility/DCM.java
+++ Backend/servRegistrarTarjeta/src/java/co/dpenad3/utility/DCM.java
@@ -13,6 +13,6 @@ public class DCM {
     
    public static String URL_BD= "jdbc:postgresql://localhost:5432/proyecto";
    public static String USUARIO_BD = "postgres";
-   public static String CONTRSEÑA_BD = "Ivonne247*";
+   public static String CONTRSENA_BD = "Ivonne247*";
    public static String DRIVER_BD = "org.postgresql.Driver";
 }
